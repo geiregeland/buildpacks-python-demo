fa0972e92d7960640d0e4a0b00db90588563afea
geiregeland <geir.egeland@gmail.com>
2023-01-05 12:48:18 +0100

    Update web.py

--- web.py
+++ web.py
@@ -19,6 +19,6 @@ def hello_user(username):
     return "Hello %s" % escape(username)
 
 
-#if __name__ == "__main__":
- #   app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0',port=8000)
     
