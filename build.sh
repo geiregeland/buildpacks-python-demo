96acaa6d771a93e5d022be75abac976e8ca846e7
geiregeland <geir.egeland@gmail.com>
2023-01-09 14:20:00 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-pack build benwilcock/python-sample:1.0.0
-docker run -d -ePORT=8080 -p8080:8080 --name python-sample benwilcock/python-sample:1.0.0
+pack build gegegeland/python-sample:1.0.0
+docker run -d -ePORT=8080 -p8080:8080 --name pysample gegegeland/python-sample:latest
 http :8080/
